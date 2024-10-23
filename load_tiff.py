ec5900798ef4a8aad8596a2f9602326b6f1abb76
Satoshi TAKAHASHI <takahashi_satoshi@prc.tsukuba.ac.jp>
2024-10-23 19:03:43 +0900

    added function load_tiff

--- load_tiff.py
+++ load_tiff.py
@@ -3,4 +3,16 @@
 """
 Reads the tiff file in [shot_no]_tif.zip
 
-""""
+"""
+
+import os
+import zipfile
+import numpy as np
+import tifffile
+
+def load_tiff(shot_no, tiff_dir, frame_tgt=0, num_frames=0):
+
+    #todo
+if __name__ == "main":
+    import matlplotlib.pyplot as plt
+    #todo
