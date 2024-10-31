2f6d596b498455107cfe479b9ef5a32abbde1d0f
Satoshi TAKAHASHI <takahashi_satoshi@prc.tsukuba.ac.jp>
2024-10-31 11:40:15 +0900

    Changed to use memmap only when it exceeeds more than 8GB

--- camera_reader.py
+++ camera_reader.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import load_tiff
 import read_conv_info
 import transform_image 
@@ -21,7 +19,7 @@ def camera_reader(shot_no, line_ch, frame_tgt=0, num_frames=0, flg_rot=False):
     Returns:
     dict: A dictionary containing processed image data and metadata.
     """
-    # config and paths
+    # Config and paths
     config_dict = read_path_info.read_path_info()
     tiff_dir = config_dict['tiff_dir']
     conv_dict = read_conv_info.read_conv_info(shot_no, tiff_dir)
@@ -29,14 +27,25 @@ def camera_reader(shot_no, line_ch, frame_tgt=0, num_frames=0, flg_rot=False):
     if num_frames == 0:
         num_frames = int(conv_dict['bottom_frame']) - int(conv_dict['top_frame']) - frame_tgt  # Load all remaining frames
     
-    # Initialize an empty array for the memmap
+    # Load the first frame to determine the shape
     first_frame = load_tiff.load_tiff(shot_no, tiff_dir, frame_tgt, 1)
     first_frame, tra_dict, coeff = transform_image.transform_image(shot_no, line_ch, tiff_dir, first_frame, flg_rot)
     
-    memmap_shape = (num_frames,) + first_frame.shape
-    memmap_filename = 'trimmed_image.dat'
-    trimmed_memmap = np.memmap(memmap_filename, dtype='float32', mode='w+', shape=memmap_shape)
-    
+    # Determine the shape and estimated memory usage
+    frame_shape = first_frame[0].shape
+    estimated_size_gb = (num_frames * frame_shape[0] * frame_shape[1] * 4) / (1024 ** 3)  # 4 bytes per float32 pixel
+
+    # estimated_size_gb = 9
+    # Initialize data storage based on estimated size
+    if estimated_size_gb > 8:
+        memmap_filename = 'trimmed_image.dat'
+        trimmed_memmap = np.memmap(memmap_filename, dtype='float32', mode='w+', shape=(num_frames,) + frame_shape)
+        camera_data = trimmed_memmap
+        print("Using memmap due to large data size")
+    else:
+        camera_data = np.empty((num_frames,) + frame_shape, dtype='float32')
+        print("Using in-memory array")
+
     # Process each frame
     for i in tqdm(range(frame_tgt, frame_tgt + num_frames), desc="Loading frames..."):
         try:
@@ -48,41 +57,44 @@ def camera_reader(shot_no, line_ch, frame_tgt=0, num_frames=0, flg_rot=False):
         # Transform the image
         img_array, tra_dict, coeff = transform_image.transform_image(shot_no, line_ch, tiff_dir, img_array, flg_rot)
         
-        # Save the trimmed image to the memmap
-        trimmed_memmap[i - frame_tgt] = img_array
+        # Save the trimmed image to the appropriate storage (memmap or in-memory array)
+        camera_data[i - frame_tgt] = img_array
 
-    # Create a dictionary with memmap file path
-    camera_dict = {'memmap_file': memmap_filename, 'coeff': coeff, 'frame_start': frame_tgt}
+    # Create a dictionary with the data storage type
+    camera_dict = {'data': camera_data, 'coeff': coeff, 'frame_start': frame_tgt}
     camera_dict.update(conv_dict)
     camera_dict.update(tra_dict)
     camera_dict.update(config_dict)
     
     camera_dict['data_size'] = (num_frames, *camera_dict['trimmed_size'])
-    
+
     return camera_dict
-    
+
 #%% Test
 if __name__ == "__main__":
     
     import time
     import matplotlib.pyplot as plt
-    import numpy as np
     
     time_sta = time.time()
     shot_no = 256221
-    frame_tgt=0
-    num_frames=0
-    flg_rot=False
+    frame_tgt = 0
+    num_frames = 1000
+    flg_rot = False
     line_ch = '2' 
     camera_dict = camera_reader(shot_no, line_ch, frame_tgt, num_frames, flg_rot)
     
-    # Load and display the saved memmap
+    # Display the first frame
+    # if camera_dict and isinstance(camera_dict['data'], np.memmap):
+    #     trimmed_memmap = camera_dict['data']
+    #     plt.imshow(trimmed_memmap[0, :, :])  # [frame, x, z]
+    #     plt.show()
+    # elif camera_dict:
+    #     plt.imshow(camera_dict['data'][0, :, :])
+    #     plt.show()
     if camera_dict:
-        memmap_file = camera_dict['memmap_file']
-        shape=(camera_dict['data_size'])
-        trimmed_memmap = np.memmap(memmap_file, dtype=np.float32, mode='r', shape=(camera_dict['data_size']))  # Adjust dtype and shape as per actual data
-        plt.imshow(trimmed_memmap[0,:,:]) # [frame, x, z]
+        plt.imshow(camera_dict['data'][0, :, :])
         plt.show()
     
     time_end = time.time()
-    print('Time spent: ' + str(time_end-time_sta) + ' (s)')
+    print('Time spent: ' + str(time_end - time_sta) + ' (s)')
